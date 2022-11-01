60c13fa781f55b08d68c730d6ff9426bf224ff46
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-01 11:29:17 +0000

    Add new var, log result

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,14 @@
 // from lesson on destructuring; 
 
-const person {
+const person = {
+  firstName: "BigDave",
+  age: 30,
+  favColor: "green",
 }
+const names = person.firstName;
+
+// how do you get the first name from the object and store?
+
+console.log(names)
+
+
