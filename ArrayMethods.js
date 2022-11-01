58a6de0aad0a230d14e87fff675c6174d9206730
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-01 12:20:23 +0000

    Used filter on array

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,9 +1,17 @@
-
 // first I want to create a new array of objects: 
 
 const newArray = [
   {name: 'steve', age: 16},
   {name: 'alan', age: 27},
-  {name: 'benito', age: 43},
+  {name: 'benito', age: 45},
   {name: 'mackers', age: 45}
 ]
+
+//filter: new array based on age from the orginal - it is nondestructive, dont want to mutate orginal
+
+//first see if I can log each object to the console: 
+const ages = newArray.filter((callwhatever) => {
+console.log(callwhatever);
+})
+
+//cool but how might I get this by age for example
