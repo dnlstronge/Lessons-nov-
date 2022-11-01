43640b059a973de4a0212b6ba529dfccac4f5083
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-01 12:29:49 +0000

    filtered based on boolean for age

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -9,9 +9,21 @@ const newArray = [
 
 //filter: new array based on age from the orginal - it is nondestructive, dont want to mutate orginal
 
-//first see if I can log each object to the console: 
+/*first see if I can log each object to the console: 
 const ages = newArray.filter((callwhatever) => {
 console.log(callwhatever);
-})
+}) */
+// if item returns true it is filtered into new array!
+
 
 //cool but how might I get this by age for example
+
+const ages = newArray.filter((callwhatever) => {
+return callwhatever.age === 45;
+
+})
+
+console.log(ages)
+
+/* filter returned benito and mackers who are both 45, this is because
+the return statement in ages was true, I say do you follow.....*/
