ac9082adb47977c5882ca4824ed13f016a04cffa
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-11-01 12:13:03 +0000

    Created array of obj

---
+++ ArrayMethods.js
@@ -0,0 +1,9 @@
+
+// first I want to create a new array of objects: 
+
+const newArray = [
+  {name: 'steve', age: 16},
+  {name: 'alan', age: 27},
+  {name: 'benito', age: 43},
+  {name: 'mackers', age: 45}
+]
